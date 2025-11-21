7b554062b3a5f218cadb14ee8b12c026a303f954
znn316 <zhangning2509@gmail.com>
2025-11-21 06:32:04 +0000

    1

--- app.py
+++ app.py
@@ -6,6 +6,11 @@ app = Flask(__name__)
 def index():
     return(render_template("index.html"))
 
+@app.route("/main",methods=["GET","POST"])
+def main():
+    q = request.form.get("q")
+    print(q)
+    return(render_template("main.html"))
 
 if __name__ == "__main__":
     app.run()
